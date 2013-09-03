30dea855d2f3c08504cbe8b94655d23d334d93a0
eettlin <mrumrocks@gmail.com>
2013-09-03 16:43:08 -0700

    Play Area resizw wo waypoints

---
+++ PhatKids/src/pkids/PKPlayArea.java
@@ -0,0 +1,62 @@
+package pkids;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import jgame.Context;
+import jgame.GContainer;
+import jgame.GObject;
+import jgame.GSprite;
+import jgame.ImageCache;
+import jgame.listener.TimerListener;
+
+public class PKPlayArea extends GContainer {
+	public PKPlayArea() {
+		// TODO Auto-generated constructor stub
+		setSize(800, 600);
+		this.setBackgroundColor(Color.BLACK);
+
+		BufferedImage bg = ImageCache.forClass(PhatKids.class).get("bg2.png");
+		GSprite bi = new GSprite(bg);
+		setBackgroundSprite(bi);
+		
+		TimerListener pkEnemyTimer = new TimerListener(45) {
+
+			@Override
+			public void invoke(GObject target, Context context) {
+				addRandomEnemy();
+			}
+		};
+
+		addListener(pkEnemyTimer);
+
+	}
+	
+	private void addRandomEnemy() {
+
+		int enemyPick = (int) (Math.random() * 3);
+		Enemy e = null;
+		//
+		switch (enemyPick) {
+		case 0:
+			e = new EnemyOne();
+			break;
+
+		case 1:
+			e = new EnemyTwo();
+			break;
+
+		case 2:
+			e = new EnemyThree();
+
+			break;
+
+		default:
+			e = new EnemyTwo();
+			break;
+		}
+
+		add(e);
+
+	}
+}
--- PhatKids/src/pkids/PhatKidsGameView.java
+++ PhatKids/src/pkids/PhatKidsGameView.java
@@ -11,60 +11,27 @@ import jgame.listener.TimerListener;
 
 public class PhatKidsGameView extends GContainer {
 
-	
 	public PhatKidsGameView() {
 		// Set the size.
-		setSize(900	, 600);
-		/*
-		 * BufferedImage bi = ImageCache.forClass(PhatKids.class).get("gameover.png");
-		 *GSprite gs = new GSprite(bi);
-		 *setBackgroundSprite(gs);
-		 */
-	    BufferedImage bg = ImageCache.forClass(PhatKids.class).get("bg2.png");
-	    GSprite gs = new GSprite(bg);
-		setBackgroundSprite(gs);
+		setSize(900, 700);
 		
-		
-		TimerListener pkEnemyTimer = new TimerListener(45) {
-			
-			@Override
-			public void invoke(GObject target, Context context) {
-				addRandomEnemy();
-			}
-		};
-		
-		addListener(pkEnemyTimer);
-		
-		
-		
+		// Create play area and locate on stage
+		PKPlayArea pa = new PKPlayArea();
+		pa.setAnchorTopLeft();
+		pa.setLocation(100, 0);
+		add(pa);
+
 		
 	}
 
-	private void addRandomEnemy() {
-		
-		int enemyPick = (int)(Math.random()*3);
-		Enemy e = null;
-		// 
-		switch(enemyPick){
-		case 0:
-			 e = new EnemyOne();
-			break;
-			
-		case 1:
-			 e = new EnemyTwo();
-			break;
-			
-		case 2:
-			 e = new EnemyThree();
 
-			break;
-			
-		 default:
-			 e = new EnemyTwo();
-			break;
-		}
-		
-		add(e);
-		
-	}
 }
+
+/*
+ * BufferedImage bi =
+ * ImageCache.forClass(PhatKids.class).get("gameover.png");GSprite gs = new
+ * GSprite(bi);setBackgroundSprite(gs);
+ */
+// BufferedImage bg = ImageCache.forClass(PhatKids.class).get("bg2.png");
+// GSprite gs = new GSprite(bg);
+// setBackgroundSprite(gs);
