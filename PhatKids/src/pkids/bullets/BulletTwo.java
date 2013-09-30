8995b32901afea937da01c7e83839d443489d320
eettlin <mrumrocks@gmail.com>
2013-09-30 09:08:56 -0700

    Stop carrots from rotating (they're much more pointy this way)

--- PhatKids/src/pkids/bullets/BulletTwo.java
+++ PhatKids/src/pkids/bullets/BulletTwo.java
@@ -1,14 +1,12 @@
 package pkids.bullets;
 
-import pkids.PhatKids;
 import jgame.ImageCache;
-import jgame.controller.ConstantRotationController;
+import pkids.PhatKids;
 
 public class BulletTwo extends Bullet {
 
 	public BulletTwo() {
 		super(ImageCache.forClass(PhatKids.class).get("bullets/b2.png"), 2);
-		addController(new ConstantRotationController(3));
 	}
 	
 
