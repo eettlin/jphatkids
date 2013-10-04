25424f3f01b0fbcc96b970232f985ba3edaf7dfa
eettlin <mrumrocks@gmail.com>
2013-10-04 10:42:47 -0700

    Disallow place turrets on top of each other

--- PhatKids/src/pkids/PhatKidsGameView.java
+++ PhatKids/src/pkids/PhatKidsGameView.java
@@ -1,5 +1,7 @@
 package pkids;
 
+import java.awt.Polygon;
+
 import pkids.areas.InfoArea;
 import pkids.areas.MenuArea;
 import pkids.areas.PKPlayArea;
@@ -50,47 +52,76 @@ public class PhatKidsGameView extends GContainer {
 	}
 
 	public void initializeTurret(final Turret t) {
-		
 		if (settingTurret) {
 			return;
 		}
-		settingTurret = true;  // This is only run when init turret 
-		
+		settingTurret = true; // This is only run when init turret
 		final RangeRing rr = new RangeRing(t.getFireRange());
 		this.pa.addAtCenter(rr);
-		////
 
 		this.pa.addAtCenter(t);
-		final MouseLocationController c = new MouseLocationController();  	// create mouse location controller
-		t.addController(c);													// attach controller to turret t
+		final MouseLocationController c = new MouseLocationController(); // create
+																			// mouse
+																			// location
+																			// controller
+		t.addController(c); // attach controller to turret t
 		rr.addController(c);
-		
-			
-		final LocalClickListener dropListener = new LocalClickListener() {  // mouse click listener hears click on  turret (see below)
+
+		final int[] x = new int[] { 657, 657, 656, 654, 650, 640, 618, 593,
+				568, 545, 524, 502, 481, 459, 436, 411, 386, 360, 335, 309,
+				285, 262, 241, 221, 204, 188, 174, 162, 151, 143, 138, 135,
+				135, 137, 143, 151, 162, 174, 190, 207, 226, 247, 270, 294,
+				318, 343, 368, 393, 418, 443, 468, 492, 514, 535, 549, 555,
+				548, 526, 501, 478, 456, 434, 411, 386, 360, 335, 311, 288,
+				268, 250, 237, 228, 219, 213, 213, 216, 223, 233, 246, 264,
+				285, 308, 333, 358, 383, 407, 416, 417, 408, 385, 361, 337,
+				316, 302, 300, 304, 314, 330, 353, 378, 403, 426, 443, 459,
+				477, 501, 526, 551, 576, 601, 626, 651, 676, 701, 726, 751,
+				776, 801 };
+		final int[] y = new int[] { 4, 31, 56, 81, 106, 129, 143, 148, 147,
+				137, 123, 109, 95, 83, 73, 67, 66, 66, 68, 74, 83, 93, 107,
+				123, 142, 163, 184, 206, 229, 253, 278, 304, 330, 355, 380,
+				404, 428, 450, 470, 489, 506, 521, 533, 543, 551, 557, 559,
+				555, 551, 548, 543, 533, 520, 505, 484, 459, 434, 422, 423,
+				434, 450, 463, 474, 479, 479, 473, 464, 453, 437, 418, 396,
+				371, 347, 322, 296, 269, 244, 221, 199, 181, 165, 154, 147,
+				144, 143, 151, 175, 200, 224, 234, 242, 251, 267, 288, 313,
+				338, 361, 381, 392, 397, 396, 385, 366, 346, 328, 319, 316,
+				318, 322, 323, 321, 318, 316, 318, 316, 314, 315, 313 };
+		final Polygon p = new Polygon(x, y, 118);
+
+		final LocalClickListener dropListener = new LocalClickListener() { // mouse
+																			// click
+																			// listener
+																			// hears
+																			// click
+																			// on
+																			// turret
+																			// (see
+																			// below)
 			@Override
 			public void invoke(GObject target, Context context) {
-				target.removeController(c);  
+				for (int i = 0; i < 118; i++) {
+					if (target.distanceTo(x[i], y[i]) < 50) {
+						return;
+					}
+				}
+				for (GObject child : pa.getObjects()) {
+					if (child != t && child instanceof Turret
+							&& child.hitTest(t)) {
+						return;
+					}
+				}
+				target.removeController(c);
 				target.removeListener(this);
-				rr.removeController(c);  
+				rr.removeController(c);
 				rr.removeSelf();
 				settingTurret = false;
-				//((Turret) target).setPlaced(true); 
-				t.setPlaced(true); //because t is set to final otherwise above
-				
+				// ((Turret) target).setPlaced(true);
+				t.setPlaced(true); // because t is set to final otherwise above
 			}
 		};
 		t.addListener(dropListener);
-		
-		////
 	}
 
 }
-
-/*
- * BufferedImage bi =
- * ImageCache.forClass(PhatKids.class).get("gameover.png");GSprite gs = new
- * GSprite(bi);setBackgroundSprite(gs);
- */
-// BufferedImage bg = ImageCache.forClass(PhatKids.class).get("bg2.png");
-// GSprite gs = new GSprite(bg);
-// setBackgroundSprite(gs);
