c8276bf2d7d395afafe2e03f72da6a3bc164a5ab
eettlin <mrumrocks@gmail.com>
2013-10-16 15:55:18 -0700

    combined tweens (not chained) for hero enter

--- PhatKids/src/pkids/HeroKid.java
+++ PhatKids/src/pkids/HeroKid.java
@@ -2,6 +2,8 @@ package pkids;
 
 import jgame.GSprite;
 import jgame.ImageCache;
+import jgame.controller.Interpolation;
+import jgame.controller.MovementTween;
 import jgame.controller.ScaleTween;
 
 public class HeroKid extends GSprite{
@@ -10,8 +12,10 @@ public class HeroKid extends GSprite{
 		super(ImageCache.getSequentialImages("herokid/phatkid", 1, 30,
 				".png"));
 		
-		ScaleTween st = new ScaleTween(30, 1.5, .25);
+		ScaleTween st = new ScaleTween(75, 1.5, .25);
+		MovementTween mt = new MovementTween(80, Interpolation.EASE_IN,  350, 0);
 		addController(st);
+		addController(mt);
 
 	}
 
