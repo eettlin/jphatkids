df550d6bd59f3bb570bc2ea5de40b6ed41b03882
eettlin <mrumrocks@gmail.com>
2013-10-22 09:58:02 -0700

    Created Bank class overloaded

--- PhatKids/src/pkids/Bank.java
+++ PhatKids/src/pkids/Bank.java
@@ -1,6 +1,26 @@
 package pkids;
 
 public class Bank {
+	// mvc = model view controller
+	private int currentValue;
 	
+	public Bank() {
+		this(500);
+	}
+
+	public Bank(int initialValue) {	
+		// establish value for getters-setters
+		currentValue = initialValue;
+		// Create getters and setters to establish value for turret
+
+	}
+
+	public int getCurrentValue() {
+		return currentValue;
+	}
+
+	public void setCurrentValue(int currentValue) {
+		this.currentValue = currentValue;
+	}
 
 }
