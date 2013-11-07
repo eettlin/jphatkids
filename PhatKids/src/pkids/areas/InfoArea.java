a14b416bd61e9c1d83d76547349a4296f49d3976
eettlin <mrumrocks@gmail.com>
2013-11-07 10:38:59 -0800

    Begin info area display of bank value

--- PhatKids/src/pkids/areas/InfoArea.java
+++ PhatKids/src/pkids/areas/InfoArea.java
@@ -1,14 +1,32 @@
 package pkids.areas;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import jgame.GContainer;
+import jgame.GMessage;
 
 public class InfoArea extends GContainer {
-public InfoArea() {
-	// TODO Auto-generated constructor stub
-	setSize(400, 100);
-	this.setBackgroundColor(Color.gray);
 
-}
+	private GMessage gm;
+
+	public InfoArea() {
+		setSize(400, 100);
+
+		this.setBackgroundColor(Color.BLACK);
+
+		gm = new GMessage();
+		setDisplayValue(0);
+
+		gm.setX(50);
+		gm.setY(50);
+		add(gm);
+		gm.setColor(Color.getHSBColor(1f / 6f, 0.75f, 1f));
+		gm.setFont(new Font("Times New Roman", Font.BOLD, 24));
+	}
+
+	public void setDisplayValue(int newValue) {
+		gm.setText(Integer.toString(newValue));
+	}
+
 }
